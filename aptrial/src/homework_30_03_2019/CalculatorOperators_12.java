e720b3a4b57d79b926b6000556a869d52ccc5e2f
Archita <contactarchitap@gmail.com>
2019-03-31 17:00:43 +0100

    No. 12

--- aptrial/src/homework_30_03_2019/CalculatorOperators_12.java
+++ aptrial/src/homework_30_03_2019/CalculatorOperators_12.java
@@ -1,11 +1,11 @@
-package homework_23_03_2019;
+package homework_30_03_2019;
 
 //        Write a program for a calculator with addition, subtraction, multiplication and division methods all with parameters and use string concatenation methods.
 //        2 methods should be static and 2 methods are instance.
 
 import java.util.Scanner;
 
-public class CalculatorOperators {
+public class CalculatorOperators_12 {
 
     public static void main (String[] args){
 
@@ -16,13 +16,13 @@ public class CalculatorOperators {
         System.out.println("Welcome to mini calculator");
         System.out.println("Enter first number: ");
         float n1 = input.nextFloat();
-        System.out.print("Enter Operator: ");
+        System.out.print("Enter any Arithmetic Operator (+ - x /) : ");
         String str=input.next();
         System.out.print("Enter Second number: ");
         float n2 = input.nextFloat();
         float ans;
 
-        CalculatorOperators COP = new CalculatorOperators();
+        CalculatorOperators_12 COP = new CalculatorOperators_12();
 
         if (str.contains("+")){
             add(n1,n2);
