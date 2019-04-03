5f50dae8d695c3b645db93ab96cdd8a60652de9f
Archita <contactarchitap@gmail.com>
2019-04-03 22:47:27 +0100

    Complete Homework

--- aptrial/src/homework_30_03_2019/MarkSheet_3.java
+++ aptrial/src/homework_30_03_2019/MarkSheet_3.java
@@ -7,24 +7,24 @@ public class MarkSheet_3 {
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.print(" Enter Student Name\t  : " );
+        System.out.print(" \nEnter Student Name\t: " );
         String name = scanner.next();
-        System.out.print(" Enter Student Rollno\t: " );
-        int rollno = scanner.nextInt();
-        System.out.print(" Enter Maths Marks\t   : " );
+        System.out.print("Enter Student RollNo: " );
+        int rollNo = scanner.nextInt();
+        System.out.print("Enter Maths Marks\t: " );
         int maths = scanner.nextInt();
-        System.out.print("Enter English Marks\t  :" );
+        System.out.print("Enter English Marks\t: " );
         int english = scanner. nextInt();
-        System.out.print("Enter Science Marks\t  : " );
+        System.out.print("Enter Science Marks\t: " );
         int science = scanner.nextInt();
         int total = maths+science+english;
         int percentage= total/3;
 
         System.out.print("\n|-------------------------------------------|");
-        System.out.print("\n|\t\t\t\t Marksheet"+"\t\t\t\t\t|");
+        System.out.print("\n|\t\t\t\t MarkSheet"+"\t\t\t\t\t|");
         System.out.print("\n|-------------------------------------------|");
         System.out.print("\n|\t Name \t\t\t:\t\t"+ name+"\t\t\t|");
-        System.out.print("\n|\t Rollno\t\t\t:\t\t" + rollno+"\t\t\t|" );
+        System.out.print("\n|\t RollNo\t\t\t:\t\t" + rollNo+"\t\t\t|" );
         System.out.print("\n|-------------------------------------------|");
         System.out.print("\n|\t Subjects\t\t:  \t\tMarks\t\t\t|");
         System.out.print("\n|-------------------------------------------|");
@@ -36,7 +36,7 @@ public class MarkSheet_3 {
         System.out.print("\n|\t Percentage\t\t: \t\t" +percentage+"%\t\t\t\t|");
 
 
-        if (percentage>35)
+        if (percentage>=35)
 
         {
             System.out.print("\n|\t pass/fail\t\t:  \t\tPass\t\t\t|");
